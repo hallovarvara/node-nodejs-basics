0b065b844c1a82ffdd88fe98b0b9fd3639a075dd
Varya Deviaterikova <hallovarvara@gmail.com>
2022-06-02 18:45:24 +0100

    feat: create hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,32 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { readFileSync, existsSync } from 'fs';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILE_FOR_HASHING = 'fileToCalculateHashFor.txt';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const filePath = resolve(__dirname, 'files', FILE_FOR_HASHING);
+
+    if (!existsSync(filePath)) {
+        throw new Error(`File "${FILE_FOR_HASHING}" doesn't exist`);
+    }
+
+    try {
+        const fileBuffer = readFileSync(filePath);
+        const hashSum = createHash('sha256');
+        hashSum.update(fileBuffer);
+        const hex = hashSum.digest('hex');
+
+        console.log(`Hex of "${FILE_FOR_HASHING}" file is "${hex}"`);
+
+        return hex;
+    } catch (readErr) {
+        throw readErr;
+    }
 };
+
+calculateHash();
