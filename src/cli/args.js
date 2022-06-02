624586e84f5c3de71a77ab287aeb1ad46a223b40
Varya Deviaterikova <hallovarvara@gmail.com>
2022-06-02 17:40:34 +0100

    feat: create cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,29 @@
+const ARG_NAME_PREFIX = '--';
+
 export const parseArgs = () => {
-    // Write your code here 
+    let wasPrevArgName = false;
+
+    const userArgs = process.argv.slice(2);
+
+    const argsString = userArgs.reduce((result, argFromConsole) => {
+        let newResult = result;
+        const isArgName = argFromConsole.startsWith(ARG_NAME_PREFIX);
+        const arg = isArgName ? argFromConsole.slice(2) : argFromConsole;
+
+        if (newResult.length < 1) {
+            newResult = arg;
+        } else if (isArgName) {
+            newResult += `, ${arg}`;
+        } else if (wasPrevArgName) {
+            newResult += ` is ${arg}`;
+        }
+
+        wasPrevArgName = isArgName;
+
+        return newResult;
+    }, '');
+
+    console.log(argsString);
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,21 @@
+const ENV_PREFIX = 'RSS_';
+const ERROR_NO_ENV_VARIABLES = `\nNo environment variables with "${ENV_PREFIX}" were provided\n\nTry to run function from project root with a command like: \nRSS_test=23 RSS_hello=test RSS_UNI=show node src/cli/env.js\n\n`;
+
 export const parseEnv = () => {
-    // Write your code here 
+    const resultArr = Object.entries(process.env).reduce(
+        (result, [name, value]) => {
+            if (name.startsWith(ENV_PREFIX)) {
+                result.push(`${name}=${value}`);
+            }
+
+            return result;
+        },
+        [],
+    );
+
+    console.log(
+        resultArr.length > 0 ? resultArr.join('; ') : ERROR_NO_ENV_VARIABLES,
+    );
 };
+
+parseEnv();
