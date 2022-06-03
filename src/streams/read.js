14cbfee8d71724cb3cc41b834780813a581640db
Varya Deviaterikova <hallovarvara@gmail.com>
2022-06-03 19:05:40 +0100

    feat: create functions with streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,29 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+import { FILES_FOLDER } from '../constants.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const FILE_TO_READ = 'fileToRead.txt';
+
 export const read = async () => {
-    // Write your code here 
+    const filePath = resolve(__dirname, FILES_FOLDER, FILE_TO_READ);
+    const readStream = createReadStream(filePath, { highWaterMark: 10 });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('close', () => {
+        console.log(
+            "\n\n🎉 File was successfully read and now it's closed. Have a nice day!",
+        );
+    });
+
+    readStream.on('error', (readError) => {
+        console.error(`\n\n${readError.message}`);
+    });
 };
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,36 @@
+import { createWriteStream } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { FILES_FOLDER } from '../constants.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const FILE_TO_WRITE = 'fileToWrite.txt';
+const STOP_PHRASE = '/stop';
+
 export const write = async () => {
-    // Write your code here 
+    const filePath = resolve(__dirname, FILES_FOLDER, FILE_TO_WRITE);
+
+    const writeStream = createWriteStream(filePath, 'utf8');
+
+    process.stdout.write(`
+Welcome to stdin-writer bot!
+Just type something in console, press Enter, and bot will write it in "${FILES_FOLDER}/${FILE_TO_WRITE}" file
+If you would like to stop, type "${STOP_PHRASE}" on a new line and press Enter
+    `);
+
+    process.stdin.on('data', (chunk) => {
+        const data = chunk.toString();
+
+        if (data.slice(0, data.indexOf(' ')) === STOP_PHRASE) {
+            process.stdout.write(
+                '\n\nThanks for using stdin-writer bot! Have a great day! 👌 ',
+            );
+            process.exit();
+        }
+
+        writeStream.write(data);
+    });
 };
+
+write();
