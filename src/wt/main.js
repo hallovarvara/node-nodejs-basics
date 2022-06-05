a2546fcf9b42c29a481987f070cffcf441b6e19b
Varya Deviaterikova <hallovarvara@gmail.com>
2022-06-05 21:46:57 +0100

    feat: add worker threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,28 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const WORKER_PATH = resolve(__dirname, 'worker.js');
+const coresNumber = [...Array(cpus().length)].map((useless, i) => i + 10);
+
 export const performCalculations = async () => {
-    // Write your code here
+    const results = await Promise.all(
+        coresNumber.map(
+            (sendToWorkerNumber) =>
+                new Promise((resolve, reject) => {
+                    const worker = new Worker(WORKER_PATH, {
+                        workerData: sendToWorkerNumber,
+                    });
+
+                    worker.on('message', resolve);
+                    worker.on('error', reject);
+                }),
+        ),
+    );
+
+    console.log(results);
 };
+
+performCalculations();
