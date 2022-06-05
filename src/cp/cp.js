fc270a58018cc56176d946a94c4e256d636c2829
Varya Deviaterikova <hallovarvara@gmail.com>
2022-06-05 21:56:34 +0100

    feat: add child process basic functional

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,19 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+import { FILES_FOLDER } from '../constants.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const FILE_TO_OPERATE = 'script.js';
+const filePath = resolve(__dirname, FILES_FOLDER, FILE_TO_OPERATE);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(filePath, args);
 };
+
+spawnChildProcess([
+    'hello',
+    'some new argument',
+    'previous was fine',
+    'add more',
+]);
